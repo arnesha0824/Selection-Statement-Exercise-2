f959382cafbfee318e35612b6683c7e2443d8254
Arnesha Jackson <ArneshaJackson1@gmail.com>
2025-10-08 02:50:38 -0400

    phase 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I don't prefer math");
+                        break;
+                case "science":
+                    Console.WriteLine("Science Rules!");
+                    break;
+                case "pe":
+                case "physical education":
+                    Console.WriteLine("Gym is awesome");
+                    break;
+                case "history":
+                    Console.WriteLine("History is awesome");
+                    break;
+                case "geography":
+                    Console.WriteLine("Geography is awesome");
+                    break;
+                default:
+                    Console.WriteLine("I LOVE THAT TOO!");
+                    break;
+            }
         }
     }
 }
